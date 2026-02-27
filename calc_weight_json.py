869a400b6e4384866aa6cfbfd7dd7cca9184743e
De4dS1 <ulidroidtng@gmail.com>
2026-02-27 19:27:16 +0000

    Archivos json

---
+++ calc_weight_json.py
@@ -0,0 +1,28 @@
+import jsonFileHandler
+
+data = jsonFileHandler.readJsonFile("files/insulin.json")
+
+if data != "":
+    bInsulin = data['molecules']['bInsulin']
+    aInsulin = data['molecules']['aInsulin']
+    insulin = bInsulin + aInsulin
+    molecularWeightInsulinActual = data['molecularWeightInsulinActual']
+    
+    print('bInsulin: ' + bInsulin)
+    print('aInsulin: ' + aInsulin)
+    print('molecularWeightInsulinActual: ' + str(molecularWeightInsulinActual))
+    
+    aaWeights = data['weights']
+    aaCountInsulin = ({
+    x: float(insulin.upper().count(x)) 
+        for x in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+    })
+    molecularWeightInsulin = sum({
+    x: (aaCountInsulin[x] * aaWeights[x]) 
+        for x in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']}.values())
+    print("The rough molecular weight of insulin: " + str(molecularWeightInsulin))
+    percentError = ((molecularWeightInsulin - molecularWeightInsulinActual) / molecularWeightInsulinActual) * 100
+    
+    print("Percent error: " + str(percentError))
+else:
+    print("Error. Exiting program")
